6bca31a76dc23db90c189cf50be65bdd25e11361
Jonas Furrer <jonas.furrer@gmail.com>
2018-05-28 14:09:04 +0200

    Add ci directory to gpg secret

--- ci/mvn-deploy.sh
+++ ci/mvn-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $encrypted_67ec11152530_key -iv $encrypted_67ec11152530_iv -in codesigning.asc.enc -out codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_67ec11152530_key -iv $encrypted_67ec11152530_iv -in ci/codesigning.asc.enc -out ci/codesigning.asc -d
 	gpg --fast-import ci/codesigning.asc
 
 	mvn deploy -P ossrh --settings ci/mvnsettings.xml
