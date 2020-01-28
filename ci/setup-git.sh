94d659b8dce9be8764a6d5ef0f910cb670334f76
Jonas Furrer <jonas.furrer@namics.com>
2020-01-28 15:10:26 +0100

    Switch from Travis-CI to Github Actions

--- ci/setup-git.sh
+++ ci/setup-git.sh
@@ -4,11 +4,6 @@ git config --global user.email "oss@namics.com"
 git config --global user.name "Namics OSS CI"
 
 ORIGIN=`git config --get remote.origin.url`
-GITHUB="https://${GITHUB_TOKEN}@${ORIGIN#'https://'}"
+GITHUB="https://${REPO_TOKEN}@${ORIGIN#'https://'}"
 git remote add github $GITHUB
 git fetch github
-
-if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	git checkout -qf $TRAVIS_BRANCH
-fi;
-
