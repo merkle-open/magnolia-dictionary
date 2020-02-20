7022bdf4430abf3712790d02962ea5dc2186c4fa
Jonas Furrer <jonas.furrer@namics.com>
2020-02-20 13:11:43 +0100

    Refactor the import/export functionality

--- src/main/java/com/namics/oss/magnolia/dictionary/DictionaryConfiguration.java
+++ src/main/java/com/namics/oss/magnolia/dictionary/DictionaryConfiguration.java
@@ -1,5 +1,9 @@
 package com.namics.oss.magnolia.dictionary;
 
+import info.magnolia.jcr.util.NodeTypes;
+
+import java.util.List;
+
 public final class DictionaryConfiguration {
 	public static final String REPOSITORY = "dictionary";
 	public static final String NODE_TYPE = "mgnl:label";
@@ -11,4 +15,25 @@ public final class DictionaryConfiguration {
 		public static final String EXPIRED = "expired";
 	}
 
+	public static final class ImportExport {
+		public static final String JCR_NAME = "jcrName";
+		public static final String REP_ROOT = "rep:root";
+		public static final String FILENAME_TEMPLATE = "export-{0}-{1}.xlsx";
+		public static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd-HHmmss";
+		public static final String SHEET_NAME = "export";
+		public static final List<String> STATIC_EXPORT_PROPERTIES = List.of(
+				JCR_NAME,
+				Prop.NAME,
+				Prop.VALUE
+		);
+		public static final List<String> DATE_EXPORT_PROPERTIES = List.of(
+				NodeTypes.Created.NAME,
+				NodeTypes.LastModified.NAME,
+				NodeTypes.Activatable.LAST_ACTIVATED
+		);
+		public static final List<String> STATUS_EXPORT_PROPERTIES = List.of(
+				NodeTypes.Activatable.ACTIVATION_STATUS
+		);
+		public static final List<String> STATIC_IMPORT_PROPERTIES = STATIC_EXPORT_PROPERTIES;
+	}
 }
--- src/main/java/com/namics/oss/magnolia/dictionary/actions/importexport/XlsExportAction.java
+++ src/main/java/com/namics/oss/magnolia/dictionary/actions/importexport/XlsExportAction.java
@@ -1,92 +1,65 @@
 package com.namics.oss.magnolia.dictionary.actions.importexport;
 
 import com.namics.oss.magnolia.dictionary.services.XlsExportService;
+import com.namics.oss.magnolia.dictionary.util.NodeUtil;
 import com.vaadin.server.Page;
 import com.vaadin.server.StreamResource;
-import info.magnolia.jcr.util.NodeUtil;
+import com.vaadin.ui.Notification;
+import info.magnolia.ui.AlertBuilder;
 import info.magnolia.ui.api.action.AbstractAction;
 import info.magnolia.ui.api.action.ActionExecutionException;
-import info.magnolia.ui.api.context.UiContext;
 import info.magnolia.ui.vaadin.integration.jcr.JcrItemAdapter;
-import info.magnolia.ui.vaadin.overlay.MessageStyleTypeEnum;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.collections4.IteratorUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.jcr.Item;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static com.namics.oss.magnolia.dictionary.DictionaryConfiguration.ImportExport.REP_ROOT;
+
 public class XlsExportAction extends AbstractAction<XlsExportActionDefinition> {
-	List<JcrItemAdapter> items;
-	List<Node> nodes;
-	UiContext uiContext;
-	XlsExportService exportService;
-	String workspace = StringUtils.EMPTY;
-	SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+	private List<Node> nodes;
+	private XlsExportService exportService;
 
-	public XlsExportAction(XlsExportActionDefinition definition, JcrItemAdapter item, UiContext uiContext, XlsExportService exportService) {
-		this(definition, Collections.singletonList(item), uiContext, exportService);
-	}
-
-	public XlsExportAction(XlsExportActionDefinition definition, List<JcrItemAdapter> items, UiContext uiContext, XlsExportService exportService) {
+	public XlsExportAction(XlsExportActionDefinition definition, List<JcrItemAdapter> items, XlsExportService exportService) {
 		super(definition);
-		this.items = items;
-		this.uiContext = uiContext;
 		this.exportService = exportService;
-		setNodes(items);
-		setWorkspace(items);
+		this.nodes = setNodes(items);
 	}
 
-	private void setWorkspace(List<JcrItemAdapter> items) {
-		if (CollectionUtils.isNotEmpty(items)) {
-			this.workspace = items.get(0).getWorkspace();
+	private List<Node> setNodes(List<JcrItemAdapter> items) {
+		if (CollectionUtils.isEmpty(items)) {
+			return Collections.emptyList();
 		}
-	}
 
-	private void setNodes(List<JcrItemAdapter> items) {
-		if (CollectionUtils.isNotEmpty(items)) {
-			if (items.size() > 1) {
-				this.nodes = itemsToNodes(items);
-			} else {
-				if (isRootNode(items.get(0))) {
-					itemToNode(items.get(0)).ifPresent(node -> {
-						try {
-							this.nodes = IteratorUtils.toList(NodeUtil.getNodes(node, getDefinition().getNodeType()).iterator());
-						} catch (RepositoryException e) {
-							uiContext.openNotification(MessageStyleTypeEnum.ERROR, true, "Error reading nodes.");
-						}
-					});
-				} else {
-					this.nodes = itemsToNodes(items);
-				}
-			}
-		} else {
-			this.nodes = Collections.emptyList();
+		if (items.size() > 1) {
+			return itemsToNodes(items);
 		}
+
+		if (!isRootNode(items.get(0))) {
+			return itemsToNodes(items);
+		}
+
+		return itemToNode(items.get(0))
+				.map(node -> NodeUtil.getNodes(node, getDefinition().getNodeType()))
+				.map(NodeUtil::asList)
+				.orElse(Collections.emptyList());
 	}
 
 	@Override
 	public void execute() throws ActionExecutionException {
-		try (ByteArrayOutputStream outputStream = exportService.exportXls(nodes, getDefinition().getProperties())) {
-			StreamResource.StreamSource source = (StreamResource.StreamSource) () -> new ByteArrayInputStream(outputStream.toByteArray());
-
-			String filename = "export-" + this.workspace + "-" + df.format(new Date()) + ".xlsx";
-			StreamResource resource = new StreamResource(source, filename);
-			resource.setCacheTime(-1);
-			resource.getStream().setParameter("Content-Disposition", "attachment; filename=" + filename + "\"");
-			Page.getCurrent().open(resource, "", true);
+		try (ByteArrayOutputStream outputStream = exportService.exportXls(nodes)) {
+			StreamResource resource = exportService.getDownloadStream(outputStream);
+			Page.getCurrent().open(resource, StringUtils.EMPTY, Boolean.TRUE);
 		} catch (IOException | RepositoryException e) {
-			uiContext.openNotification(MessageStyleTypeEnum.ERROR, true, "Error creating xls File for download.");
+			showErrorMessage();
 		}
 	}
 
@@ -95,7 +68,6 @@ public class XlsExportAction extends AbstractAction<XlsExportActionDefinition> {
 		if (jcrItem.isNode()) {
 			return Optional.of((Node) jcrItem);
 		}
-
 		return Optional.empty();
 	}
 
@@ -111,12 +83,20 @@ public class XlsExportAction extends AbstractAction<XlsExportActionDefinition> {
 		Item jcrItem = item.getJcrItem();
 		if (jcrItem.isNode()) {
 			try {
-				return ((Node) jcrItem).getPrimaryNodeType().isNodeType("rep:root");
+				return NodeUtil.isNodeType((Node) jcrItem, REP_ROOT);
 			} catch (RepositoryException e) {
 				return false;
 			}
 		}
-
 		return false;
+	}
+
+	private void showErrorMessage() {
+		AlertBuilder
+				.alert("Error creating xls file for download.")
+				.withLevel(Notification.Type.ERROR_MESSAGE)
+				.withTitle("Error creating xls file for download.")
+				.withBody("Could not create xls file.")
+				.buildAndOpen();
 	}
 }
