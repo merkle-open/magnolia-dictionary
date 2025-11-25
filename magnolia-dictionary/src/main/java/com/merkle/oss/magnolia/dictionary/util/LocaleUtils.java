47d3541763231b8554d0949b8d15a95ecbd6bc70
eschleb <bjoern.eschle@merkle.com>
2025-11-25 10:20:25 +0100

    Fix multi site module not loaded issue

--- magnolia-dictionary/src/main/java/com/merkle/oss/magnolia/dictionary/util/LocaleUtils.java
+++ magnolia-dictionary/src/main/java/com/merkle/oss/magnolia/dictionary/util/LocaleUtils.java
@@ -1,35 +1,44 @@
 package com.merkle.oss.magnolia.dictionary.util;
 
+import info.magnolia.cms.i18n.I18nContentSupport;
 import info.magnolia.module.site.Site;
 import info.magnolia.module.site.SiteManager;
 import info.magnolia.objectfactory.Components;
 
-import java.util.ArrayList;
+import java.lang.invoke.MethodHandles;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author mrauch, Namics AG
  * @since 15.03.2016
  */
 public class LocaleUtils {
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	public static String getLocaleString(Locale locale) {
 		return locale.toString();
 	}
 
 	public static List<Locale> getLocalesOfAllSiteDefinitions() {
-		List<Locale> locales = new ArrayList<>();
-
-		SiteManager manager = Components.getComponent(SiteManager.class);
-		for (Site site : manager.getSites()) {
-			if (site.getI18n() != null) {
-				site.getI18n().getLocales().stream()
-						.filter(locale -> !locales.contains(locale))
-						.forEach(locales::add);
-			}
-		}
-
-		return locales;
+        try {
+            final SiteManager manager = Components.getComponent(SiteManager.class);
+            return manager.getSites()
+                    .stream()
+                    .map(Site::getI18n)
+                    .map(I18nContentSupport::getLocales)
+                    .flatMap(Collection::stream)
+                    .distinct()
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            LOG.error("Failed to get locales of all sites", e);
+            return Collections.emptyList();
+        }
 	}
 }
